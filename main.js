e69dbc1588def7a141d26308ec0500294699b6da
andor <latandor@gmail.com>
2022-06-29 12:21:47 -0400

    pokemon challange 1

--- main.js
+++ main.js
@@ -8,38 +8,56 @@ const userRoster = []
 const containerDiv = document.querySelector(`#container`)
 const newBtn = document.querySelector(`#new-pokemon-btn`)
 const rosterDiv = document.querySelector(`#roster`)
+const removeBtn = document.querySelector(`#remove-pokemon-btn`)
 
+function addZeroes(numAdded) {
+   if (numAdded.length === 1) {
+     return `00`+ numAdded
+   } else if (numAdded.length === 2) {
+     return `0` + numAdded
+   } else {
+     return numAdded
+   }
+}
+ 
 newBtn.addEventListener(`click`, async () => {
-let num = prompt(`ENTER A POKEMON NUMBER`)
-console.log(`number entered`, num)
-let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${num}.png`
-let dataUrl = `https://pokeapi.co/api/v2/pokemon/${num}`
-let req = await fetch(dataUrl)
-let res = await req.json()
-let name = res.forms[0].name
-let audioUrl = `https://play.pokemonshowdown.com/audio/cries/${name.toLowerCase()}.mp3`
-let audio = document.createElement(`audio`)
-let source = document.createElement(`source`)
-source.setAttribute(`src`, audioUrl)
-source.setAttribute(`type`, `audio/mpeg`) 
-audio.append(source)
-let h3 = document.createElement(`h3`)
-h3.innerText = name.toUpperCase()
-let img = document.createElement(`img`)
-img.setAttribute(`src`, imgUrl)
-img.setAttribute(`class`, `roster-img`)
-let position = document.querySelector(`#pokemon-${userRoster.length + 1}`)
-position.addEventListener(`click`, () => {
- audio.play()
-})
-position.append(img, h3, audio)
-userRoster.push(num)
+ if (userRoster.length < 6) {
+  let num = prompt(`ENTER A POKEMON NUMBER`)
+  console.log(`number entered`, num)
+  addedNum = addZeroes(num)
+  let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${addedNum}.png`
+  let dataUrl = `https://pokeapi.co/api/v2/pokemon/${num}`
+  let req = await fetch(dataUrl)
+  let res = await req.json()
+  let name = res.forms[0].name
+  let audioUrl = `https://play.pokemonshowdown.com/audio/cries/${name.toLowerCase()}.mp3`
+  let audio = document.createElement(`audio`)
+  let source = document.createElement(`source`)
+  source.setAttribute(`src`, audioUrl)
+  source.setAttribute(`type`, `audio/mpeg`) 
+  audio.append(source)
+  let h3 = document.createElement(`h3`)
+  h3.innerText = name
+  let img = document.createElement(`img`)
+  img.setAttribute(`src`, imgUrl)
+  img.setAttribute(`class`, `roster-img`)
+  let position = document.querySelector(`#pokemon-${userRoster.length + 1}`)
+  position.addEventListener(`click`, () => {
+  audio.play()
+  })
+  position.append(img, h3, audio)
+  userRoster.push(num)
+}
 })
 
+removeBtn.addEventListener(`click`, function() {
+
+ })
 // loop over every ID 
 // create an HTML element
 // set the HTML element values
 // append HTML element to the DOM
+
 
 pokemon.map((element, index) => { // index is a counter variable
 console.log(element) //fragile code
@@ -63,3 +81,4 @@ img.src = imgUrl
 div.append(img, h3) 
 containerDiv.append(div)
 })
+
